c84708160d976788dcd56d8575ddd5978f8eba12
srschroll <sschroll@stumail.fhtc.edu>
2016-08-25 16:28:14 -0600

    Add files via upload

---
+++ RagEngine.java
@@ -0,0 +1,50 @@
+/* 
+ * Ragnaros (c) 2016
+ * Developed, Designed and produced by Shane R Schroll
+ * Last Update: August 25, 2016
+ * 
+ * 
+ * Ragnaros Game Engine  RagEngine_Version 1
+ * Built for 2D, top-down development
+ * Designed by Shane R Schroll
+*/
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class RagEngine extends JFrame
+{
+	/* Entry point */
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	
+	/* This method starts the game and loops it */
+	public void run()
+	{
+		init();
+		
+	}
+	
+	
+	/* This method sets up things needed for the game to run */
+	void init()
+	{
+		
+	}
+	
+	/* This method checks for input, win/loss conditions, moves objects around, etc */
+	void update()
+	{
+		
+	}
+	
+	
+	/* This method draws everything */
+	void draw()
+	{
+		
+	}
+}
